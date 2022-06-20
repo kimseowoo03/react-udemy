cd71d24cc74355f2c74e638ebb1b3d006bfc7d3a
kimseowoo03 <kimseowoo03@gmail.com>
2022-06-20 23:17:00 +0900

    정적 -> 동적표현

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,18 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-  return(
-    <h2>Expense item!</h2>
-  )
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = '스타벅스 아메리카노'
+  const expenseAmount = 4500;
+  return (
+    <div className='expense-item'>
+      <div>{expenseDate.toISOString()}</div>
+      <div className='expense-item_description'>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item_price'>{expenseAmount}원</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
