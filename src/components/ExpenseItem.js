ea2cd88d07bf8648f55e34f36157e7b3ed0f5903
kimseowoo03 <kimseowoo03@gmail.com>
2022-06-21 23:00:35 +0900

    3단계 컴포넌트 분리

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,14 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = '스타벅스 아메리카노'
-  const expenseAmount = 4500;
+function ExpenseItem(props) {
+
   return (
     <div className='expense-item'>
-      <div>{expenseDate.toISOString()}</div>
+      <ExpenseDate date={props.date}/>
       <div className='expense-item_description'>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item_price'>{expenseAmount}원</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item_price'>{props.amount}</div>
       </div>
     </div>
   );
