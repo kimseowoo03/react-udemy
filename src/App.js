8abd64fb572d9db945a242c95d0c9001d2b0ee58
kimseowoo03 <kimseowoo03@gmail.com>
2022-07-31 23:46:40 +0900

    모달창 생성 및 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 const App = () => {
   return (
     <React.Fragment>
@@ -8,6 +9,7 @@ const App = () => {
       <main>
         <Meals />
       </main>
+      <Cart />
     </React.Fragment>
   );
 };
