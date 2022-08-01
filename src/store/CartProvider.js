ef492c31ae90c5353ab04830fee23cf01e69456a
kimseowoo03 <kimseowoo03@gmail.com>
2022-08-01 22:15:40 +0900

    useReducer을 사용하여 현재 상태와 업데이트 될 상태 관리

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,14 +1,38 @@
 import CartContext from "./cart-context";
+import { useReducer } from "react";
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0
+}
+
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedItems = state.items.concat(action.item);
+    const updateTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updateTotalAmount,
+    };
+  }
+  return defaultCartState;
+};
 
 const CartProvider = props => {
 
-  const addItemToCartHandler = item => {};
+  const [cartState, setCartState] = useReducer(cartReducer, defaultCartState)
 
-  const removeItemFromCartHandler = item => {};
+  const addItemToCartHandler = item => {
+    setCartState({ type: 'ADD', item: item});
+  };
+
+  const removeItemFromCartHandler = id => {
+    setCartState({ type: 'REMOVE', id: id});
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   } ;
